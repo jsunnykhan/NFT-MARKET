590edd1c3430fb9d7b44f4acf81525ba355d057a
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-12 13:03:09 +0600

    Added collection support

---
+++ components/MakeCollectionModal.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import { Market_ADDRESS } from '../config';
+import { ethers } from 'ethers';
+
+import NftContract from '../artifacts/contracts/NFT.sol/NFT.json';
+
+const MakeCollectionModal = (props) => {
+  const { getEvents, isCollectionModalOpen, setIsCollectionModalOpen } = props;
+
+  const [valid, setValid] = useState(true);
+  const [formInput, setFormInput] = useState({
+    collection: '',
+    symbol: '',
+  });
+
+  const handleContractCreation = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const Nft = new ethers.ContractFactory(
+      NftContract.abi,
+      NftContract.bytecode,
+      signer
+    );
+    const nft = await Nft.deploy(
+      Market_ADDRESS,
+      formInput.collection,
+      formInput.symbol
+    );
+    await nft.deployTransaction.wait();
+    const nftAddress = nft.address;
+    setIsCollectionModalOpen(false);
+    getEvents();
+  };
+
+  useEffect(() => {
+    if (!isCollectionModalOpen) {
+      console.log(true);
+      return () => true;
+    }
+  }, [isCollectionModalOpen]);
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 z-30 w-full h-full bg-black bg-opacity-60 min-w-full min-h-screen">
+      <div className="relative w-max h-max mt-40 m-auto items-center bg-white ring-1 ring-purple-100 rounded p-5 mb-3">
+        <p
+          className="absolute top-0 right-3 font-bold cursor-pointer p-2"
+          onClick={() => setIsCollectionModalOpen(false)}
+        >
+          x
+        </p>
+        <h3 className="font-semibold text-xl text-gray-700">
+          Create New Collection
+        </h3>
+        <div className="grid gap-4 grid-cols-2 grid-rows-1 mt-4">
+          <input
+            type="text"
+            required
+            className="ring-1 ring-gray-300  bg-none  px-3 py-3 rounded w-full transition-all duration-300 hover:shadow-md outline-none focus:ring-gray-400"
+            placeholder="Collection Name"
+            onChange={(event) =>
+              setFormInput((preState) => ({
+                ...preState,
+                collection: event.target.value,
+              }))
+            }
+          />
+          <input
+            type="text"
+            required
+            className="ring-1 ring-gray-300  bg-none  px-3 py-3 rounded w-full transition-all duration-300 hover:shadow-md outline-none focus:ring-gray-400"
+            placeholder="Collection Symbol"
+            onChange={(event) =>
+              setFormInput((preState) => ({
+                ...preState,
+                symbol: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <button
+          className="w-1/2 bg-blue-600 text-white text-xl font-semibold py-2.5 rounded-lg mt-10 disabled:bg-blue-200"
+          // disabled={valid}
+          onClick={handleContractCreation}
+        >
+          Create Collection
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MakeCollectionModal;

9d35fde294f62d568dafcc0e73c66a068086363a
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-16 19:26:11 +0600

    Changed to non strict mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["upload.wikimedia.org", "ipfs.infura.io"],
   },

d7b3cb6453bee3c8b3d9bcecb4a93a68d60b6448
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-16 19:25:09 +0600

    Updated for nft.storage and https conversion

--- src/helper/upload.js
+++ src/helper/upload.js
@@ -1,12 +1,12 @@
-import { NFTStorage, File } from "nft.storage";
-import mime from "mime";
+import { NFTStorage, File } from 'nft.storage';
+import mime from 'mime';
 
 const NFT_STORAGE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY4MUI0MjAwMDJhNDU0YzJmRDk1NTFmOGE0QTdENkVBODQzRDRiOTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDE5MTE2NjI2MiwibmFtZSI6Ik1hcmtldFBsYWNlIn0.PRmPgRxYx-0AZIg8YCP3-ltZHOAd6GB9HoAEE3taazg";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY4MUI0MjAwMDJhNDU0YzJmRDk1NTFmOGE0QTdENkVBODQzRDRiOTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDE5MTE2NjI2MiwibmFtZSI6Ik1hcmtldFBsYWNlIn0.PRmPgRxYx-0AZIg8YCP3-ltZHOAd6GB9HoAEE3taazg';
 
 export const uploadFile = async (file) => {
   console.log(file);
-  let url = "";
+  let url = '';
   const client = new NFTStorage({ token: NFT_STORAGE_KEY });
   const cid = await client.storeBlob(file);
   url = `https://ipfs.io/ipfs/${cid}`;
@@ -24,6 +24,33 @@ export const uploadMetaData = async (metaData, file) => {
     properties: metaData.attributes,
   });
   console.log(metadata);
-  console.log(metadata.url);
+  const ipfsLink = metadata.data.image.href;
+  console.log(metadata.data.image.href);
+  const tempUrl = ipfsLink.slice(7, ipfsLink.length);
+  const finalUrl = 'https://nftstorage.link/ipfs/' + tempUrl;
+  console.log(finalUrl);
   return metadata.url;
 };
+
+/*
+https://bafybeiazxj5ftmh526gxjn3rowrnl5mrjflg3sxbqk32od4fc6lnk3ct4q/wallpaperflare.com_wallpaper (4).jpg.ipfs.nftstorage.link
+
+
+ipfs://bafybeiazxj5ftmh526gxjn3rowrnl5mrjflg3sxbqk32od4fc6lnk3ct4q/wallpaperflare.com_wallpaper (4).jpg
+
+https://nftstorage.link/ipfs/bafybeiazxj5ftmh526gxjn3rowrnl5mrjflg3sxbqk32od4fc6lnk3ct4q/wallpaperflare.com_wallpaper (4).jpg
+
+https://nftstorage.link/ipfs/bafybeiazxj5ftmh526gxjn3rowrnl5mrjflg3sxbqk32od4fc6lnk3ct4q/wallpaperflare.com_wallpaper
+*/
+
+
+// which one shall i use? ipfs or http
+// i can convert from ipfs to http
+
+
+/**
+ *
+ * ipfs://bafyreibi3fvnfhapsc346uzp6qs2ogby4h2vdiisglt7khwv4unj5iupfq/metadata.json
+ *
+ * https://nftstorage.link/ipfs/bafyreibi3fvnfhapsc346uzp6qs2ogby4h2vdiisglt7khwv4unj5iupfq/metadata.json
+ */

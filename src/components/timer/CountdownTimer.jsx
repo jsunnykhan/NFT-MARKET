834dac436c1efe24156144f5a9f160f7c4040faf
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-17 17:03:49 +0600

    Added countdown timer

---
+++ src/components/timer/CountdownTimer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useCountdown } from '../../helper/hooks/useCountdown';
+import ShowCounter from './ShowCounter';
+
+const CountdownTimer = ({ targetDate, id, itemId }) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+  if (days + hours + minutes + seconds === 0) {
+    return <ShowCounter days={0} hours={0} minutes={0} seconds={0} />;
+  } else if (days + hours + minutes + seconds < 0) {
+    return <ShowCounter days={0} hours={0} minutes={0} seconds={0} />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+    );
+  }
+};
+
+export default CountdownTimer;

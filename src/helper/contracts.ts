d02b3e74ca6e9103ad462147e05e39a3746ae4e6
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-18 19:03:43 +0600

    Refactor and side effects

--- src/helper/contracts.ts
+++ src/helper/contracts.ts
@@ -1,24 +1,34 @@
-
-
-import { ethers } from "ethers";
-import { NFT_ADDRESS, collection, Market_ADDRESS, market, ERC20_TOKEN, token, AUCTION_MARKET, auction } from "./contractImport";
+import { ethers } from 'ethers';
+import {
+  NFT_ADDRESS,
+  collection,
+  Market_ADDRESS,
+  market,
+  ERC20_TOKEN,
+  token,
+  AUCTION_MARKET,
+  auction,
+} from './contractImport';
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 
-const URL: string = 'http://127.0.0.1:8545';
-
 const _getProvider = (): ethers.providers.JsonRpcProvider => {
-  const jsonRpcProvider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(URL);
+  const jsonRpcProvider: ethers.providers.JsonRpcProvider =
+    new ethers.providers.JsonRpcProvider(baseURL);
   return jsonRpcProvider;
-}
+};
 
-export const _getCollectionContract = (address = NFT_ADDRESS): ethers.Contract => {
-  const contract: ethers.Contract = new ethers.Contract(address, collection.abi, _getProvider());
+export const _getCollectionContract = (
+  address = NFT_ADDRESS
+): ethers.Contract => {
+  const contract: ethers.Contract = new ethers.Contract(
+    address,
+    collection.abi,
+    _getProvider()
+  );
   return contract;
-
-}
-
+};
 
 export const _getMarketContract = (): ethers.Contract => {
   const contract: ethers.Contract = new ethers.Contract(

9817863a97e5e027aa9e3e99f959f542cc6d63db
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-22 12:53:16 +0600

    Added auto config file creation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,12 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
+const fs = require('fs');
 
 async function main() {
-  const NFtMarket = await hre.ethers.getContractFactory("NFTMarket");
-  const Auction = await hre.ethers.getContractFactory("AuctionMarket");
+  const NFtMarket = await hre.ethers.getContractFactory('NFTMarket');
+  const Auction = await hre.ethers.getContractFactory('AuctionMarket');
 
-  const VSCoin = await hre.ethers.getContractFactory("VSCoin");
-  const Nft = await hre.ethers.getContractFactory("Collection");
+  const VSCoin = await hre.ethers.getContractFactory('VSCoin');
+  const Nft = await hre.ethers.getContractFactory('Collection');
 
   const nftMarket = await NFtMarket.deploy(10);
   await nftMarket.deployed();
@@ -15,13 +16,28 @@ async function main() {
   await vsCoin.deployed();
   const vsCoinAddress = vsCoin.address;
 
-  const nft = await Nft.deploy("Default collection", "DFC", nftMarket.address);
+  const nft = await Nft.deploy('Default collection', 'DFC', nftMarket.address);
   await nft.deployed();
   const nftAddress = nft.address;
 
   const auction = await Auction.deploy();
   await auction.deployed();
   const auctionAddress = auction.address;
+
+  const contracts = {
+    nftMarketAddress,
+    nftAddress,
+    vsCoinAddress,
+    auctionAddress,
+  };
+
+  const configFIle = `export const Market_ADDRESS = "${nftMarketAddress}";
+  export const NFT_ADDRESS = "${nftAddress}";
+  export const ERC20_TOKEN = "${vsCoinAddress}";
+  export const AUCTION_MARKET = "${auctionAddress}";
+  `;
+
+  fs.writeFileSync('./config.js', configFIle);
 
   console.log({ nftMarketAddress, nftAddress, vsCoinAddress, auctionAddress });
 }

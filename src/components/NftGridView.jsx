6198494f3acb2b5d2f3b5b0b46cc4f17d5e2099d
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-08-22 15:04:44 +0600

    Updated for current UI

--- src/components/NftGridView.jsx
+++ src/components/NftGridView.jsx
@@ -1,30 +1,24 @@
-import Link from "next/link";
-import React from "react";
-import SingleGridView from "./SingleGridView";
+import Link from 'next/link';
+import React from 'react';
+import SingleGridView from './SingleGridView';
 
 const NftGridView = (props) => {
-  const { nftList, notFoundMessage, redirectNftDetailPage } = props;
+  const { nftList, redirectNftDetailPage } = props;
   return (
     <div className="w-full">
-      {!nftList.length ? (
-        <p className="flex text-center mx-auto justify-center">
-          {notFoundMessage}
-        </p>
-      ) : (
-        <div className="w-full h-full grid grid-cols-4 gap-5 lg:gap-10">
-          {nftList.map((item) => (
-            <div key={item.tokenId} onClick={() => redirectNftDetailPage(item)}>
-              <SingleGridView
-                key={item.tokenId}
-                nft={item}
-                isBuy={false}
-                processing={false}
-                buyNFT={() => {}}
-              />
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {nftList.map((item) => (
+          <div key={item.tokenId} onClick={() => redirectNftDetailPage(item.tokenId, item.collectionAddress)}>
+            <SingleGridView
+              key={item.tokenId}
+              nft={item}
+              isBuy={false}
+              processing={false}
+              buyNFT={() => {}}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
